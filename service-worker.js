379079cffaee4ccb1a0f9d30a8eddf6e06deac55
pezzaliapp <pezzalialessandro@gmail.com>
2025-10-26 14:56:30 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,20 @@
-const CACHE = 'qa3d-v1.0.2';
+const CACHE = 'qa3d-v1.1.3';
 const APP_SHELL = [
-  './',
-  './index.html',
-  './styles.css',
-  './app.js',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './','./index.html','./styles.css','./app.js',
+  './manifest.json','./icons/icon-192.png','./icons/icon-512.png'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL)));
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)));
 });
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE && caches.delete(k)))));
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE && caches.delete(k)))));
 });
-
-// Try cache first, then network; cache CDN modules after first load
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', e=>{
   const url = new URL(e.request.url);
   if (url.origin === location.origin) {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+    e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
   } else {
-    e.respondWith(
-      caches.match(e.request).then(res => res || fetch(e.request).then(resp => {
-        const copy = resp.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-        return resp;
-      }).catch(()=>res))
-    );
+    e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
   }
 });
